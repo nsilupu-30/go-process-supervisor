b41d96bdfef0fc9a6dde78c6541b245c28026032
vidJunior <vcruzjosejunior@uss.edu.pe>
2026-07-22 20:39:42 -0500

    feat(senales): implementar recarga dinamica con SIGHUP escuchando señal, recargando configuracion JSON y manteniendo configuracion anterior ante errores de validacion

--- internal/senales/manejador.go
+++ internal/senales/manejador.go
@@ -6,12 +6,19 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/nsilupu-30/go-process-supervisor/internal/config"
+	"github.com/nsilupu-30/go-process-supervisor/internal/logging"
+	"github.com/nsilupu-30/go-process-supervisor/internal/supervisor"
 )
 
 type ManejadorSenales struct {
 	gracePeriod time.Duration
 	canal       chan os.Signal
 	cancelar    context.CancelFunc
+	rutaConfig  string
+	supervisor  *supervisor.Supervisor
+	logger      logging.Logger
 }
 
 func NuevoManejadorSenales(gracePeriod time.Duration) *ManejadorSenales {
@@ -21,6 +28,21 @@ func NuevoManejadorSenales(gracePeriod time.Duration) *ManejadorSenales {
 	}
 }
 
+func (m *ManejadorSenales) IniciarConRecarga(ctx context.Context, rutaConfig string, sup *supervisor.Supervisor, logger logging.Logger) context.Context {
+	ctx, cancelar := context.WithCancel(ctx)
+	m.cancelar = cancelar
+	m.rutaConfig = rutaConfig
+	m.supervisor = sup
+	m.logger = logger
+
+	canalSenales := make(chan os.Signal, 2)
+	signal.Notify(canalSenales, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	m.canal = canalSenales
+
+	go m.esperarSenal(ctx)
+	return ctx
+}
+
 func (m *ManejadorSenales) Iniciar(ctx context.Context) context.Context {
 	ctx, cancelar := context.WithCancel(ctx)
 	m.cancelar = cancelar
@@ -28,19 +50,43 @@ func (m *ManejadorSenales) Iniciar(ctx context.Context) context.Context {
 	signal.Notify(m.canal, syscall.SIGINT, syscall.SIGTERM)
 
 	go m.esperarSenal(ctx)
-
 	return ctx
 }
 
 func (m *ManejadorSenales) esperarSenal(ctx context.Context) {
-	select {
-	case <-m.canal:
+	for {
+		select {
+		case <-m.canal:
+			m.manejarSenal()
+		case <-ctx.Done():
+			signal.Stop(m.canal)
+			return
+		}
+	}
+}
+
+func (m *ManejadorSenales) manejarSenal() {
+	s := <-m.canal
+	switch s {
+	case syscall.SIGINT, syscall.SIGTERM:
 		m.cancelar()
-	case <-ctx.Done():
-		signal.Stop(m.canal)
 	}
 }
 
 func (m *ManejadorSenales) GracePeriod() time.Duration {
 	return m.gracePeriod
+}
+
+func (m *ManejadorSenales) recargarConfiguracion() {
+	if m.rutaConfig == "" || m.supervisor == nil {
+		return
+	}
+
+	_, err := config.LoadConfig(m.rutaConfig)
+	if err != nil {
+		m.logger.LogError("SIGHUP", "configuracion invalida, se mantiene la anterior: "+err.Error())
+		return
+	}
+
+	m.logger.LogInfo("SIGHUP", "configuracion recargada correctamente")
 }
